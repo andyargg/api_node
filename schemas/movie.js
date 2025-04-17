75800a0eb10bff83a0048b5262e5c75d49abaee8
andyargg <andresarguin04@gmail.com>
2025-04-17 18:40:55 -0300

    update

--- schemas/movie.js
+++ schemas/movie.js
@@ -1,28 +1,25 @@
-const z = require('zod')
+import { z } from 'zod';
 
 const movieSchema = z.object({
-        title: z.string({
-            invalid_type_error: "Movie title must be a string",
-            required_error: "movie title is required"
-        }),
-        year: z.number().int().positive().min(1900).max(2026),
-        director: z.string(),
-        duration: z.number().int().positive(),
-        rate: z.number().int().positive().min(0).max(10).default(0),
-        poster: z.string().url(),
-        genre: z.enum(['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi', 'Crime']).array(),
-        
-})
+    title: z.string({
+        invalid_type_error: "Movie title must be a string",
+        required_error: "movie title is required"
+    }),
+    year: z.number().int().positive().min(1900).max(2026),
+    director: z.string(),
+    duration: z.number().int().positive(),
+    rate: z.number().int().positive().min(0).max(10).default(0),
+    poster: z.string().url(),
+    genre: z.array(
+        z.enum(['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi', 'Crime'])
+    )
+});
 
-function validateMovie(input) {
-    return movieSchema.safeParse(input)
+export function validateMovie(input) {
+    return movieSchema.safeParse(input);
 }
 
-function validatePartialMovie(input) {
-    return movieSchema.partial().safeParse(input)
+export function validatePartialMovie(input) {
+    return movieSchema.partial().safeParse(input);
 }
 
-module.exports = {
-    validateMovie,
-    validatePartialMovie,
-}
